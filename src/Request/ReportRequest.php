2ad2fdbd2ddcb55753e9dbbae3001e2793e848d5
yuxiufang <374768663@qq.com>
2022-04-23 16:28:13 +0800

    report

---
+++ src/Request/ReportRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AdolphYu\WalmartMarketplace\Request;
+
+
+class ReportRequest extends Request
+{
+    public function __construct($config)
+    {
+        parent::__construct($config);
+    }
+
+    public function getReport($param)
+    {
+        return $this->getAuthRequest()
+            ->get('/v3' . $this->getCountry() . '/getReport', $param);
+    }
+
+
+    public function availableReconReportDates($param)
+    {
+        return $this->getAuthRequest()
+            ->get('/v3/' . $this->getCountry() . 'report/reconreport/availableReconFiles', $param);
+    }
+
+    public function reconReport($param)
+    {
+        return $this->getAuthRequest()
+            ->get('/v3' . $this->getCountry() . '/report/reconreport/reconFile', $param);
+    }
+
+
+}
