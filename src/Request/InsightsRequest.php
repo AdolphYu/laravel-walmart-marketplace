863b313a86fa194752cff15c4b7d3dbfed17dd73
yuxiufang <374768663@qq.com>
2022-06-07 10:43:07 +0800

    insights

--- src/Request/InsightsRequest.php
+++ src/Request/InsightsRequest.php
@@ -9,6 +9,38 @@ class InsightsRequest extends Request
             ->get('/v3'.$this->getCountry().'/insights/items/trending',$param);
 
     }
+    public function unpublishedItemCounts($param=[]){
+        return  $this->getAuthRequest()
+            ->get('/v3'.$this->getCountry().'/insights/items/unpublished/counts',$param);
+    }
+
+    public function unpublishedItems($param=[]){
+        return  $this->getAuthRequest()
+            ->get('/v3'.$this->getCountry().'/insights/items/unpublished/items',$param);
+    }
+
+    public function sellerListingQualityScore($param=[]){
+        return  $this->getAuthRequest()
+            ->get('/v3'.$this->getCountry().'/insights/items/listingQuality/score',$param);
+    }
+
+    public function itemListingQualityDetails($param=[]){
+        return  $this->getAuthRequest()
+            ->post('/v3'.$this->getCountry().'/insights/items/listingQuality/items',$param);
+    }
+
+    public function itemCountWithListingQualityIssues($param=[]){
+        return  $this->getAuthRequest()
+            ->get('/v3'.$this->getCountry().'/insights/items/listingQuality/count',$param);
+    }
+    public function proSellerBadgeStatus($param=[]){
+        return  $this->getAuthRequest()
+            ->get('/v3'.$this->getCountry().'/insights/prosellerbadge',$param);
+    }
+
+
+
+
 
 
 }
